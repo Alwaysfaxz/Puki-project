37ad841d234d344f6777fa7eb358b9e8cadde546
Alwaysfaxz <faxz4you@gmail.com>
2024-09-21 18:43:30 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,241 @@
+Skip to content
+Navigation Menu
+
+Code
+Issues
+Pull requests
+BreadcrumbsPuki-Project
+/install.sh
+Latest commit
+Alwaysfaxz
+Alwaysfaxz
+last month
+History
+485 lines (442 loc) · 17.7 KB
+File metadata and controls
+
+Code
+
+Blame
+hackback_panel() {
+#!/bin/bash
+
+# Color
+BLUE='\033[0;34m'       
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+# Display welcome message
+display_welcome() {
+  echo -e ""
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]                                                 [+]${NC}"
+  echo -e "${BLUE}[+]                AUTO INSTALLER THEMA             [+]${NC}"
+  echo -e "${BLUE}[+]                  © IBZZ OFFCIAL                [+]${NC}"
+  echo -e "${BLUE}[+]                                                 [+]${NC}"
+  echo -e "${RED}[+] =============================================== [+]${NC}"
+  echo -e ""
+  echo -e "script ini di buat untuk mempermudah penginstalasian thema pterodactyle,"
+  echo -e "dilarang keras untuk memperjual belikan."
+  echo -e ""
+  echo -e "𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 :"
+  echo -e "@IbzzWangsaff"
+  echo -e "𝗖𝗥𝗘𝗗𝗜𝗧𝗦 :"
+  echo -e "© Ibzz - Official"
+  sleep 4
+  clear
+}
+
+#Update and install jq
+install_jq() {
+  echo -e "                                                       "
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]             UPDATE & INSTALL JQ                 [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  sudo apt update && sudo apt install -y jq
+  if [ $? -eq 0 ]; then
+    echo -e "                                                       "
+    echo -e "${GREEN}[+] =============================================== [+]${NC}"
+    echo -e "${GREEN}[+]              INSTALL JQ BERHASIL                [+]${NC}"
+    echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  else
+    echo -e "                                                       "
+    echo -e "${RED}[+] =============================================== [+]${NC}"
+    echo -e "${RED}[+]              INSTALL JQ GAGAL                   [+]${NC}"
+    echo -e "${RED}[+] =============================================== [+]${NC}"
+    exit 1
+  fi
+  echo -e "                                                       "
+  sleep 1
+  clear
+}
+#Check user token
+check_token() {
+  echo -e "                                                       "
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]               LICENSY IBZZ OFFICIAL             [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
+  read -r USER_TOKEN
+
+  if [ "$USER_TOKEN" = "IBZZ AZAH" ]; then
+    echo -e "${GREEN}AKSES BERHASIL${NC}}"
+  else
+    echo -e "${GREEN}Buy dulu Gih Ke Ibzz Official${NC}"
+    echo -e "${YELLOW}TELEGRAM : @IbzzWangsaff${NC}"
+    echo -e "${YELLOW}WHATSAPP : 6289601071334${NC}"
+    echo -e "${YELLOW}HARGA TOKEN : 10K FREE UPDATE JIKA ADA TOKEN BARU${NC}"
+    echo -e "${YELLOW}©IbzzOffcial${NC}"
+    exit 1
+  fi
+  clear
+  echo -e "${BLUE}[+]                    UNINSTALL PANEL                 [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+
+
+bash <(curl -s https://pterodactyl-installer.se) <<EOF
+y
+y
+y
+y
+EOF
+
+
+  echo -e "                                                       "
+  echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  echo -e "${GREEN}[+]                 UNINSTALL PANEL SUKSES             [+]${NC}"
+  echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  sleep 2
+  clear
+  exit 0
+}
+configure_wings() {
+  echo -e "                                                       "
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]                    CONFIGURE WINGS                 [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  #!/bin/bash
+
+# Minta input token dari pengguna
+read -p "Masukkan token Configure menjalankan wings: " wings
+
+eval "$wings"
+# Menjalankan perintah systemctl start wings
+sudo systemctl start wings
+
+  echo -e "                                                       "
+  echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  echo -e "${GREEN}[+]                 CONFIGURE WINGS SUKSES             [+]${NC}"
+  echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  sleep 2
+  clear
+  exit 0
+}
+hackback_panel() {
+  echo -e "                                                       "
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]                    HACK BACK PANEL                 [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  # Minta input dari pengguna
+read -p "Masukkan Username Panel: " user
+read -p "password login " psswdhb
+  #!/bin/bash
+cd /var/www/pterodactyl || { echo "Direktori tidak ditemukan"; exit 1; }
+
+# Membuat lokasi baru
+php artisan p:user:make <<EOF
+yes
+hackback@gmail.com
+$user
+$user
+$user
+$psswdhb
+EOF
+  echo -e "                                                       "
+  echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  echo -e "${GREEN}[+]                 AKUN TELAH DI ADD             [+]${NC}"
+  echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  sleep 2
+  
+  exit 0
+}
+# Main script
+display_welcome
+install_jq
+check_token
+
+while true; do
+  clear
+  echo -e "                                                                     "
+  echo -e "${RED}        _,gggggggggg.                                     ${NC}"
+  echo -e "${RED}    ,ggggggggggggggggg.                                   ${NC}"
+  echo -e "${RED}  ,ggggg        gggggggg.                                 ${NC}"
+  echo -e "${RED} ,ggg'               'ggg.                                ${NC}"
+  echo -e "${RED}',gg       ,ggg.      'ggg:                               ${NC}"
+  echo -e "${RED}'ggg      ,gg'''  .    ggg       Auto Installer Ibzz Official   ${NC}"
+  echo -e "${RED}gggg      gg     ,     ggg      ------------------------  ${NC}"
+  echo -e "${RED}ggg:     gg.     -   ,ggg       • Telegram : Ibzzwangsaff      ${NC}"
+  echo -e "${RED} ggg:     ggg._    _,ggg        • Creadit  : IBZZ OFFICIAL  ${NC}"
+  echo -e "${RED} ggg.    '.'''ggggggp           • Support by IBZZ OFFICIAL  ${NC}"
+  echo -e "${RED}  'ggg    '-.__                                           ${NC}"
+  echo -e "${RED}    ggg                                                   ${NC}"
+  echo -e "${RED}      ggg                                                 ${NC}"
+  echo -e "${RED}        ggg.                                              ${NC}"
+  echo -e "${RED}          ggg.                                            ${NC}"
+  echo -e "${RED}             b.                                           ${NC}"
+  echo -e "                                                                     "
+  echo -e "BERIKUT LIST INSTALL :"
+  echo "1. Install theme"
+  echo "2. Uninstall theme"
+  echo "3. Configure Wings"
+  echo "4. Create Node"
+  echo "5. Uninstall Panel"
+  echo "6. Stellar Theme"
+  echo "7. Hack Back Panel"
+  echo "x. Exit"
+  echo -e "Masukkan pilihan 1/2/x:"
+  read -r MENU_CHOICE
+  clear
+
+  case "$MENU_CHOICE" in
+    1)
+      install_theme
+      ;;
+    2)
+      uninstall_theme
+      ;;
+      3)
+      configure_wings
+      ;;
+      4)
+      create_node
+      ;;
+      5)
+      uninstall_panel
+      ;;
+      6)
+      install_themeSteeler
+      ;;
+      7)
+      hackback_panel
+      ;;
+    x)
+      echo "Keluar dari skrip."
+      exit 0
+      ;;
+    *)
+      echo "Pilihan tidak valid, silahkan coba lagi."
+      ;;
+  esac
+done
+Puki-Project/install.sh at main · Alwaysfaxz/Puki-Project
